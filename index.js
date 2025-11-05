39ea61261dbf961c3b06f1787ee1b0f357fc3252
casimmons-byte <casimmons@anamosa.k12.ia.us>
2025-11-05 23:59:34 +0000

    quotes

---
+++ index.js
@@ -0,0 +1,7 @@
+let introduction = 'hello how doing,\n' +
+"\t pumpkin \"day is\" over.\n" +
+'i miss scary pumpkin  \'day\'.\n\n' +
+'go see the pumpkins\n' +
+'might even hear somone say PUMPKIN :\\'
+
+document.getElementById("result").innerHTML = introduction;
